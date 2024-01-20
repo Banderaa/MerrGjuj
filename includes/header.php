9e4fd74c13763be2bdd83817acf32444e7562b8b
Rektorofubt <nadidida2@gmail.com>
2024-01-20 21:53:33 +0100

    Added Login and logout

--- includes/header.php
+++ includes/header.php
@@ -1,73 +1,76 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="assets/images/icon.png" type="image/png" />
-    <title>Merregjuj</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
 
-    <!-- CDN -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
-      integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
-      integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="assets/images/icon.png" type="image/png" />
+  <title>Merregjuj</title>
+  <link rel="stylesheet" href="assets/css/style.css" />
 
-    <!-- font awesome cdn  -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-      integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-  </head>
+  <!-- CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <body>
-    <header>
-      <div class="logo">
-        <a href="index.php"
-          ><img src="assets/images/Logo.png" alt="merregjuj" width="200px"
-        /></a>
-      </div>
-      <div class="link">
-        <ul>
-          <li>
-            <a href="index.php">Home</a>
-          </li>
-          <li>
-            <a href="about-us.php">About us</a>
-          </li>
-          <li>
-            <a href="contact-us.php">Contact us</a>
-          </li>
-          <li>
-            <a href="">Paketat</a>
-          </li>
-        </ul>
-      </div>
+  <!-- font awesome cdn  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <a href="index.php"><img src="assets/images/Logo.png" alt="merregjuj" width="200px" /></a>
+    </div>
+    <div class="link">
+      <ul>
+        <li>
+          <a href="index.php">Home</a>
+        </li>
+        <li>
+          <a href="about-us.php">About us</a>
+        </li>
+        <li>
+          <a href="contact-us.php">Contact us</a>
+        </li>
+        <li>
+          <a href="">Paketat</a>
+        </li>
+      </ul>
+    </div>
+    <?php
+    if (isset($_SESSION['user_id'])) {
+
+    ?>
       <div class="login">
         <ul>
           <li>
-            <a href="login.php"
-              ><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a
-            >
+            <a href="login.php"><i class="fa-solid fa-user"></i> <?php echo $_SESSION['username']; ?></a>
           </li>
           <li>
-            <a href="register.php"
-              ><i class="fa-solid fa-user-plus"></i> Register</a
-            >
+            <a href="includes/logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
           </li>
         </ul>
       </div>
-    </header>
+
+    <?php
+    } else {
+    ?>
+
+      <div class="login">
+        <ul>
+          <li>
+            <a href="login.php"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+          </li>
+          <li>
+            <a href="register.php"><i class="fa-solid fa-user-plus"></i> Register</a>
+          </li>
+        </ul>
+      </div>
+    <?php
+    }
+    ?>
+
+  </header>
--- login.php
+++ login.php
@@ -1,15 +1,49 @@
 <?php
 require_once "includes/header.php";
+require "includes/db.php";
+
+if (isset($_POST['submit'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (empty($username) || empty($password)) {
+    if (empty($username)) {
+      echo '<script> alert("Username is empty!")</script>';
+    }
+
+    if (empty($password)) {
+      echo '<script> alert("Password is empty!")</script>';
+    }
+  } else {
+    $encrypted_password = md5($password);
+    $sql = "SELECT id, username FROM users WHERE (username = '$username' OR email = '$username') AND password = '$encrypted_password'";
+    $result = $conn->query($sql);
+    if ($result) {
+      if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+
+        header("Location: index.php");
+        exit();
+      } else {
+        echo '<script> alert("Invalid username or password!")</script>';
+      }
+    } else {
+      echo '<script> alert("Error while login, please try again!")</script>';
+    }
+  }
+}
 ?>
 <div class="form">
   <img src="assets/images/register.jpg" alt="" />
-  <form id="loginForm" action="" onsubmit="return validateForm()">
+  <form id="loginForm" method="post">
     <input type="text" name="username" id="username" placeholder="Username" />
     <input type="password" name="password" id="password" placeholder="Password" />
 
     <span id="setError" class="error"></span>
     <br />
-    <button type="submit">Login</button>
+    <button type="submit" name="submit">Login</button>
   </form>
 </div>
 
